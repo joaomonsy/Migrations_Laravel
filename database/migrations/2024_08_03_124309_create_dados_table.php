ea85250bb79c58a64deed0eb996bd93a5102b66f
joaomonsy <joaomonsinhatti@gmail.com>
2024-08-07 11:59:57 -0300

    second commit

---
+++ database/migrations/2024_08_03_124309_create_dados_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dados', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('telefone', 14);
+            $table->string('email', 50)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('motivocontato', 255);
+            $table->string('contato', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dados');
+    }
+}
